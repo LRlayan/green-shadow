3f0bf9fb934c031317f070e25bafcee033095fa4
LRLayan <rameshlayan4@gmail.com>
2024-10-29 00:10:06 +0530

    display none and block

---
+++ controller/IndexController.js
@@ -0,0 +1,6 @@
+$('#header-sec').css({display:'none'});
+
+$('#btn-signIn').on('click',function (){
+    $('#header-sec').css({display: 'block'})
+    $('#signInAndSignUp-sec').css({display: 'none'})
+});
