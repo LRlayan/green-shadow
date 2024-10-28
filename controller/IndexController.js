3923ac9f9f81e3652a7f8e258b38efa4023942a5
LRLayan <rameshlayan4@gmail.com>
2024-10-29 00:29:26 +0530

    logout button function

--- controller/IndexController.js
+++ controller/IndexController.js
@@ -4,3 +4,8 @@ $('#btn-signIn').on('click',function (){
     $('#header-sec').css({display: 'block'})
     $('#signInAndSignUp-sec').css({display: 'none'})
 });
+
+$('#btn-logout').on('click',function (){
+    $('#header-sec').css({display:'none'})
+    $('#signInAndSignUp-sec').css({display:'block'})
+});
