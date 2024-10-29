bcf16c42edfab673e03704ee43b83eb6a9d438ec
LRLayan <rameshlayan4@gmail.com>
2024-10-29 20:49:22 +0530

    section block and none

--- controller/IndexController.js
+++ controller/IndexController.js
@@ -7,16 +7,32 @@ $('#staff-sec').css({display:'none'});
 $('#monitoring-log-sec').css({display:'none'});
 $('#vehicle-sec').css({display:'none'});
 $('#equipment-sec').css({display:'none'});
+$('#sections-wrapper').css({display:'none'});
 
 $('#btn-signIn').on('click',function (){
-    $('#header-sec').css({display: 'block'})
+    $('#sections-wrapper').css({display:'block'});
+    $('#header-sec').css({display: 'block'});
     $('#dashboard-sec').css({display:'block'});
-    $('#signInAndSignUp-sec').css({display: 'none'})
+    $('#signInAndSignUp-sec').css({display: 'none'});
+    $('#field-sec').css({display:'none'});
+    $('#crops-sec').css({display:'none'});
+    $('#staff-sec').css({display:'none'});
+    $('#monitoring-log-sec').css({display:'none'});
+    $('#vehicle-sec').css({display:'none'});
+    $('#equipment-sec').css({display:'none'});
 });
 
 $('#btn-logout').on('click',function (){
-    $('#header-sec').css({display:'none'})
     $('#signInAndSignUp-sec').css({display:'block'})
+    $('#sections-wrapper').css({display:'none'});
+    $('#header-sec').css({display:'none'});
+    $('#dashboard-sec').css({display:'none'});
+    $('#field-sec').css({display:'none'});
+    $('#crops-sec').css({display:'none'});
+    $('#staff-sec').css({display:'none'});
+    $('#monitoring-log-sec').css({display:'none'});
+    $('#vehicle-sec').css({display:'none'});
+    $('#equipment-sec').css({display:'none'});
 });
 
 $('#dashboard').on('click',function (){
