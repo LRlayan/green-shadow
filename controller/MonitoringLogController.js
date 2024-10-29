b7f740b979e8230b9aa2b073f76d5f4a5da9d7dc
LRLayan <rameshlayan4@gmail.com>
2024-10-30 01:26:17 +0530

    Function to add crops Ids

--- controller/MonitoringLogController.js
+++ controller/MonitoringLogController.js
@@ -9,3 +9,15 @@ $('#addLogFieldButton').on('click', function() {
         filedInput.val(''); // Clear the input field
     }
 });
+
+// Function to add crops Ids
+$('#addLogCropButton').on('click', function() {
+    const cropInput = $('#log-cropId');
+    const cropValue = cropInput.val().trim();
+    if (cropValue) {
+        const cropListDiv = $('#logCropList');
+        const cropElement = $('<div></div>').text(cropValue);
+        cropListDiv.append(cropElement);
+        cropInput.val(''); // Clear the input field
+    }
+});
