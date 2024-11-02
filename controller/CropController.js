801b4d76edd7b98dbe58af9d830d161371ffeb61
LRLayan <rameshlayan4@gmail.com>
2024-11-02 20:59:36 +0530

    remove code

--- controller/CropController.js
+++ controller/CropController.js
@@ -1,19 +1,3 @@
-export function previewCropImage(inputId, previewId) {
-    const input = $(`#${inputId}`)[0];
-    const preview = $(`#${previewId}`);
-
-    if (input.files && input.files[0]) {
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-            preview.attr('src', e.target.result);
-            preview.removeClass('d-none'); // Show the image preview
-        };
-
-        reader.readAsDataURL(input.files[0]); // Convert the image file to base64
-    }
-}
-
 $(document).ready(function() {
 // Function to add field Ids
     $('#addCropFieldButton').on('click', function() {
