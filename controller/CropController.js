c5e683f0dae3bfcbd57d7cfcd31d0a3ea1659de3
LRLayan <rameshlayan4@gmail.com>
2024-11-10 20:20:21 +0530

    add property in card

--- controller/CropController.js
+++ controller/CropController.js
@@ -283,6 +283,8 @@ export class LoadCards{
                                     <p class="card-scientific"><strong>Scientific Name:</strong> ${crop.scientificName}</p>
                                     <p class="card-category"><strong>Category:</strong> ${crop.category}</p>
                                     <p class="card-season"><strong>Crop Season:</strong> ${crop.season}</p>
+                                    <p class="card-FieldId"><strong>Field ID:</strong> ${crop.fieldList.join(', ')}</p>
+                                    <p class="card-logs"><strong>Logs:</strong> ${crop.logList.join(', ')}</p>
                                     <div class="d-flex justify-content-between">
                                         <button class="btn btn-success flex-grow-1 me-2 update-button" data-card-id="card${index}">Update</button>
                                         <button class="btn btn-danger flex-grow-1 delete-button" data-bs-toggle="modal" data-card-id="card${index}" data-bs-target="#confirmCropDeleteModal">Delete</button>
