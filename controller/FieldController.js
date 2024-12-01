f05295db55acc7ff31c17e766d9355fc047da3ac
LRLayan <rameshlayan4@gmail.com>
2024-12-02 01:11:57 +0530

    handleError

--- controller/FieldController.js
+++ controller/FieldController.js
@@ -440,11 +440,43 @@ export class LoadFieldCard {
                     resolve(fieldCodes);
                 },
                 error: function (xhr, status, error) {
-                    alert("Failed to retrieve fields");
+                    handleError(xhr);
                     reject(error);
                 }
             });
         });
+    }
+}
+
+function handleError(xhr) {
+    switch (xhr.status) {
+        case 400:
+            Swal.fire("Bad Request", "The request was invalid. Please check your input and try again.", "error");
+            break;
+        case 401:
+            Swal.fire("Unauthorized", "You are not authorized to perform this action.", "warning");
+            break;
+        case 403:
+            Swal.fire("Forbidden", "You do not have permission to access this resource.", "error");
+            Swal.fire({
+                title: "The Session expired?",
+                text: "You have to log again to system?",
+                icon: "question"
+            }).then(() => {
+                window.location.replace('index.html');
+                localStorage.removeItem("securityKey")
+                localStorage.removeItem("jwtToken")
+            });
+            break;
+        case 404:
+            Swal.fire("Not Found", "The requested resource could not be found.", "info");
+            break;
+        case 500:
+            Swal.fire("Server Error", "An error occurred on the server. Please try again later.", "error");
+            break;
+        default:
+            Swal.fire("Error", "An unexpected error occurred. Please try again.", "error");
+            break;
     }
 }
 
